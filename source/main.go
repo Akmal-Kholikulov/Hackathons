20ed1df9b0fc3a12e8efd2142a2be22119deb68e
Akmal <akmal.kholikulov@gmail.com>
2021-02-12 18:11:25 +0300

    updates while

---
+++ source/main.go
@@ -0,0 +1,21 @@
+n = int(input())
+c = 1
+while c <= n:
+    print('*' * c)
+    c += 1
+
+m = int(input())
+stars = '*'
+while len(stars) <= m:
+    print(stars)
+    stars += '*'
+
+
+i = 0
+while i < 5:
+    print('*')
+    if i % 2 == 0:
+        print('**')
+    if i > 2:
+        print('***')
+    i = i + 1
